4a67ede885cbcf93ed93d84d87fc291fad21da94
depp1993 <singh.deepankar93@gmail.com>
2016-01-11 22:58:14 +0530

    Login base activity

--- app/src/main/java/com/codecaine/mpurse/activity/LoginActivity.java
+++ app/src/main/java/com/codecaine/mpurse/activity/LoginActivity.java
@@ -1,31 +1,27 @@
-package com.codecaine.mpurse;
+package com.codecaine.mpurse.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.codecaine.mpurse.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
